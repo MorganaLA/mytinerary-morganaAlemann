0fad8b9717b2ba70feaeb33eb316cffe2604e5e1
MorganaLA <moriluz97@gmail.com>
2023-08-05 21:44:10 -0300

    Menu & MenuH

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ export default {
       },
       screens: {
         'sm': '400px',
-        'md': '768px',
+        'md': '700px',
         'lg': '1024px',
         'xl': '1280px',
         '2xl': '1536px',
