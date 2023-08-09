97a386c01dc0a72bf465f10ac03f16b170f57390
MorganaLA <moriluz97@gmail.com>
2023-08-09 11:11:04 -0300

    hot fix carousel

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -6,7 +6,7 @@ export const Cities = () => {
         <div className="text-center bg-indigo-600/30 my-4 max-h-full">
             <h2 className="text-3xl my-4">Cities</h2>
             <div className="flex flex-row items-center justify-center">
-                <p className="italic text-2xl my-4">Incoming...</p>
+                <p className="italic text-2xl my-4">Coming soon...</p>
                 <img className="w-8 h-8 p-px animate-beat" src="./../LogoF.png" alt="Logo" />
             </div>
         </div>
