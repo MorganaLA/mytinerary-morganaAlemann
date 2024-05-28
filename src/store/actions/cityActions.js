84a65f860b4d2ad8e3641e51c9bb49abd8a253b0
Morgana Alemann <109295512+MorganaLA@users.noreply.github.com>
2024-05-28 16:57:37 -0300

    Update cityActions.js

--- src/store/actions/cityActions.js
+++ src/store/actions/cityActions.js
@@ -1,29 +1,30 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+const baseURL = 'https://mytinerary-back-morganaalemann.onrender.com';
+
 export const get_cities = createAsyncThunk('get_cities', async () => {
     try {
-        const response = await axios.get('http://localhost:8000/api/cities')
+        const response = await axios.get(`${baseURL}/api/cities`);
 
         return {
             cities: response.data.cities
-        }
+        };
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-})
+});
 
 export const filter_cities = createAsyncThunk('filter_cities', async (obj) => {
     try {
-        const response = await axios.get(`http://localhost:8000/api/cities?name=${obj.name}`)
-     
+        const response = await axios.get(`${baseURL}/api/cities?name=${obj.name}`);
+
         return {
             cities: response.data.cities
-        }
+        };
     } catch (error) {
-        
         return {
             cities: []
-        }
+        };
     }
-})
+});
